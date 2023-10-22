926ff4d395fd19bc6ae7e2017673252efa294c0d
Ushchenko <danyodesa@gmail.com>
2023-10-22 19:07:54 +0300

    lab6

---
+++ 2-SoftwareDesign/2.4-DeatailedDataSchema/RelationDB_DetailedDataSchema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE User (
+    login VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (login)
+);
+
+CREATE TABLE Product (
+    name VARCHAR(255) NOT NULL,
+    manufacturer VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (name, manufacturer)
+);
+
+CREATE TABLE Order (
+    active BOOLEAN NOT NULL,
+    count INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
+    product_manufacturer VARCHAR(255) NOT NULL,
+    PRIMARY KEY (product_name, product_manufacturer),
+    FOREIGN KEY (product_name, product_manufacturer) REFERENCES Product(name, manufacturer)
+);
+
+
+CREATE TABLE Consumer (
+    order_id INT PRIMARY KEY,
+    FOREIGN KEY (order_id) REFERENCES Order(order_id)
+);
